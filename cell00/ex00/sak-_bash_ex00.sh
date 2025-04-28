d36f2ce20396e8aac1940a67304a172dd438925b
sak- <detectiveleeassociatespi@gmail.com>
2025-04-28 13:27:10 +0700

    Add files via upload

---
+++ cell00/ex00/sak-_bash_ex00.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir discovery_piscine
+echo"Folder discovery_piscine has been created...\n"
+cd discovery_piscine
+mkdir cell00
+echo"Folder discovery_piscine/cell00 has been created...\n"
+cd cell00
+mkdir ex00
+echo"Folder discovery_piscine/ex00 has been created...\n\n"
+pwd
