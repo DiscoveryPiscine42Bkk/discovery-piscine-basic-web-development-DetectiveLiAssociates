003e91c2029454a5c79ad1df4ad0682ea287571d
sak- <detectiveleeassociatespi@gmail.com>
2025-05-01 18:02:43 +0700

    Add files via upload

---
+++ cell03/ex01/balloon.js
@@ -0,0 +1,30 @@
+    const ball=document.getElementById('balloon');
+    const col=['#ff0000', '#00ff00', '#0000ff'];
+    let size=200; let arrCol=0;
+
+    function balloon() {
+      ball.style.width=`${size}px`;
+      ball.style.height=`${size}px`;
+      ball.style.backgroundColor=col[arrCol];
+    }
+
+    ball.addEventListener('click', () => {
+      size+=10;
+      arrCol=(arrCol+1) % col.length;
+      if (size > 420) {
+        size=200; arrCol=0;
+      }
+      balloon();
+    });
+
+    ball.addEventListener('mouseover', () => {
+      size=Math.max(200, size-5);
+      arrCol--;
+      if(arrCol<0){
+         arrCol=2;                      
+      }
+      if (size < 200) {
+        size=200; arrCol=0;
+      }
+      balloon();
+    });
